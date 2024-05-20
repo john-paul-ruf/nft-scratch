d7bd623e710978389006039c0b62bd18c89b1744
john ruf <john.paul.ruf@gmail.com>
2024-05-20 07:13:09 -0500

    resume folder

---
+++ src/resume-folder.js
@@ -0,0 +1,54 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+import {Settings} from "my-nft-gen/src/core/Settings.js";
+import {LoopBuilder} from "my-nft-gen/src/core/animation/LoopBuilder.js";
+
+// src/red-eye/red-eye-jh7ilxm/red-eye-jh7ilxm-settings.json
+
+const mainMenu = () => {
+    const InitialActions = {
+        Resume: 'Resume a build',
+        Exit: 'Exit',
+    };
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'initialAction',
+            message: 'Options',
+            choices: [
+                InitialActions.Resume,
+                InitialActions.Exit,
+            ],
+        },
+    ])
+        .then((answers) => {
+            switch (answers.initialAction) {
+            case InitialActions.Exit:
+                return;
+            case InitialActions.Resume:
+                inquirer.prompt([
+                    {
+                        name: 'filename',
+                        message: 'Input settings file name',
+                    },
+                ])
+                    .then((answers) => {
+                        async function CreateLoop() {
+                            const settings = Settings.from(JSON.parse(fs.readFileSync(answers.filename)));
+                            const loopBuilder = new LoopBuilder(settings);
+                            return loopBuilder.constructLoop();
+                        }
+
+                        const promiseArray = [];
+                        promiseArray.push(CreateLoop());
+                        Promise.all(promiseArray).then(() => {
+                            mainMenu();
+                        });
+                    });
+                break;
+            }
+        });
+};
+
+mainMenu();
