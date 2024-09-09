1f366e638f8d5d4f7dfcb6f86bcb2f65ebf8b9f1
john.paul.ruf@gmail.com <john.paul.ruf@gmail.com>
2024-09-09 17:18:12 -0500

    making art

--- src/fuzz-flare.js
+++ src/fuzz-flare.js
@@ -35,7 +35,7 @@ const createComposition = async (colorScheme) => {
         artist: 'John Ruf',
         projectName: 'fuzz-flare',
         projectDirectory: 'src/fuzz-flare/',
-        neutrals: ['#00FF00'],
+        neutrals: ['#FF0000'],
         backgrounds: ['#080808'],
         numberOfFrame: 1800,
         colorScheme: colorScheme,
@@ -55,7 +55,7 @@ const createComposition = async (colorScheme) => {
                 stroke: 2,
                 thickness: 1,
                 sparsityFactor: [8],
-                innerRadius: getRandomIntInclusive(myTestProject.shortestSideInPixels * 0.1, myTestProject.shortestSideInPixels * 0.15),
+                innerRadius: getRandomIntInclusive(myTestProject.shortestSideInPixels * 0.3, myTestProject.shortestSideInPixels * 0.3),
                 outerRadius: getRandomIntInclusive(myTestProject.shortestSideInPixels * 0.70, myTestProject.shortestSideInPixels * 0.8),
                 possibleJumpRangeInPixels: {lower: 10, upper: 30},
                 lineLength: {lower: 200, upper: 300},
@@ -81,7 +81,7 @@ const createComposition = async (colorScheme) => {
                 stroke: 2,
                 thickness: 1,
                 sparsityFactor: [8],
-                innerRadius: getRandomIntInclusive(myTestProject.shortestSideInPixels * 0.1, myTestProject.shortestSideInPixels * 0.15),
+                innerRadius: getRandomIntInclusive(myTestProject.shortestSideInPixels * 0.3, myTestProject.shortestSideInPixels * 0.3),
                 outerRadius: getRandomIntInclusive(myTestProject.shortestSideInPixels * 0.70, myTestProject.shortestSideInPixels * 0.8),
                 possibleJumpRangeInPixels: {lower: 10, upper: 30},
                 lineLength: {lower: 200, upper: 300},
@@ -107,7 +107,7 @@ const createComposition = async (colorScheme) => {
                 stroke: 2,
                 thickness: 1,
                 sparsityFactor: [8],
-                innerRadius: getRandomIntInclusive(myTestProject.shortestSideInPixels * 0.1, myTestProject.shortestSideInPixels * 0.15),
+                innerRadius: getRandomIntInclusive(myTestProject.shortestSideInPixels * 0.3, myTestProject.shortestSideInPixels * 0.3),
                 outerRadius: getRandomIntInclusive(myTestProject.shortestSideInPixels * 0.70, myTestProject.shortestSideInPixels * 0.8),
                 possibleJumpRangeInPixels: {lower: 10, upper: 30},
                 lineLength: {lower: 200, upper: 300},
@@ -133,7 +133,7 @@ const createComposition = async (colorScheme) => {
                 stroke: 2,
                 thickness: 1,
                 sparsityFactor: [8],
-                innerRadius: getRandomIntInclusive(myTestProject.shortestSideInPixels * 0.1, myTestProject.shortestSideInPixels * 0.15),
+                innerRadius: getRandomIntInclusive(myTestProject.shortestSideInPixels * 0.3, myTestProject.shortestSideInPixels * 0.3),
                 outerRadius: getRandomIntInclusive(myTestProject.shortestSideInPixels * 0.70, myTestProject.shortestSideInPixels * 0.8),
                 possibleJumpRangeInPixels: {lower: 10, upper: 30},
                 lineLength: {lower: 200, upper: 300},
@@ -159,13 +159,13 @@ const createComposition = async (colorScheme) => {
                 thickness: 18,
                 ampStroke: 0,
                 ampThickness: 1,
-                radius: [300],
+                radius: [350],
                 startAngle: [270],
-                amplitude: {lower: 125, upper: 125},
-                times: {lower: 2, upper: 2},
+                amplitude: {lower: 150, upper: 150},
+                times: {lower: 4, upper: 4},
                 accentRange: {bottom: {lower: 10, upper: 10}, top: {lower: 40, upper: 40}},
-                blurRange: {bottom: {lower: 3, upper: 3}, top: {lower: 8, upper: 8}},
-                featherTimes: {lower: 2, upper: 2},
+                blurRange: {bottom: {lower: 4, upper: 4}, top: {lower: 12, upper: 12}},
+                featherTimes: {lower: 4, upper: 4},
             })
         }),
     });
@@ -178,8 +178,19 @@ const createComposition = async (colorScheme) => {
                 folderName: 'C:\\Users\\neomo\\WebstormProjects\\nft-scratch\\src\\assets\\mappedFrames\\skull-idea\\',
                 layerOpacity: [0.7],
                 buffer: [200],
-                loopTimes: 500,
+                times: 80,
             }),
+            possibleSecondaryEffects: [
+                new LayerConfig({
+                    effect: GlowEffect,
+                    percentChance: 100,
+                    currentEffectConfig: new GlowConfig({
+                        lowerRange: {lower: -220, upper: -220},
+                        upperRange: {lower: -260, upper: -260},
+                        times: {lower: 4, upper: 4},
+                    }),
+                })
+            ]
         }),
     });
 
@@ -202,34 +213,26 @@ const createComposition = async (colorScheme) => {
                     new MultiStepDefinitionConfig({
                         minPercentage: 0,
                         maxPercentage: 25,
-                        min: new Range(10, 30),
-                        max: new Range(30, 60),
+                        max: new Range(5,15),
                         times: new Range(1, 1),
-                        invert: false
                     }),
                     new MultiStepDefinitionConfig({
                         minPercentage: 25,
                         maxPercentage: 50,
-                        min: new Range(10, 30),
-                        max: new Range(30, 60),
+                        max: new Range(5,15),
                         times: new Range(1, 1),
-                        invert: false
                     }),
                     new MultiStepDefinitionConfig({
                         minPercentage: 50,
                         maxPercentage: 75,
-                        min: new Range(10, 30),
-                        max: new Range(30, 60),
+                        max: new Range(5,15),
                         times: new Range(1, 1),
-                        invert: false
                     }),
                     new MultiStepDefinitionConfig({
                         minPercentage: 75,
                         maxPercentage: 100,
-                        min: new Range(10, 30),
-                        max: new Range(30, 60),
+                        max: new Range(5,15),
                         times: new Range(1, 1),
-                        invert: false
                     })
                 ],
 
@@ -238,7 +241,7 @@ const createComposition = async (colorScheme) => {
                 flareRingStroke: new Range(2, 2),
                 flareRingThickness: new Range(1, 1),
 
-                numberOfFlareRays: new Range(50, 50),
+                numberOfFlareRays: new Range(25, 25),
                 flareRaysSizeRange: new PercentageRange(new PercentageLongestSide(0.7), new PercentageLongestSide(1)),
                 flareRaysStroke: new Range(2, 2),
                 flareRayThickness: new Range(1, 1),
@@ -256,16 +259,16 @@ const createComposition = async (colorScheme) => {
             effect: CRTScanLinesEffect,
             percentChance: 100,
             currentEffectConfig: new CRTScanLinesConfig({
-                lines: {lower: 20, upper: 20},
+                lines: {lower: 40, upper: 40},
                 loopTimes: {lower: 1, upper: 1},
-                brightness: {lower: 1500, upper: 2550},
-                thickness: {lower: 5, upper: 5},
-                lineBlur: {lower: 3, upper: 2},
+                brightness: {lower: 1000, upper: 1000},
+                thickness: {lower: 4, upper: 4},
+                lineBlur: {lower: 6, upper: 6},
             }),
         }),
     });
 
-    await myTestProject.addFinalEffect({
+    /*await myTestProject.addFinalEffect({
         layerConfig: new LayerConfig({
             effect: CRTDegaussEffect,
             percentChance: 100,
@@ -279,7 +282,7 @@ const createComposition = async (colorScheme) => {
                 maxOffset: {lower: 40, upper: 60},
             }),
         }),
-    });
+    });*/
 
     await myTestProject.addFinalEffect({
         layerConfig: new LayerConfig({
