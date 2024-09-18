2c2ac8ea30f96abb19d0f2af619dd21a71c7e4cb
john.paul.ruf@gmail.com <john.paul.ruf@gmail.com>
2024-09-18 07:52:09 -0500

    making art

--- src/key-frame.js
+++ src/key-frame.js
@@ -12,8 +12,8 @@ import {CRTShadowEffect} from "../../my-nft-gen/src/effects/finalImageEffects/cr
 import {CRTShadowConfig} from "../../my-nft-gen/src/effects/finalImageEffects/crtShadow/CRTShadowConfig.js";
 import {GlowConfig} from "../../my-nft-gen/src/effects/secondaryEffects/glow/GlowConfig.js";
 import {GlowEffect} from "../../my-nft-gen/src/effects/secondaryEffects/glow/GlowEffect.js";
-import {CRTDegaussEffect} from "../../my-nft-gen/src/effects/finalImageEffects/crtDegaussEvent/CRTDegaussEffect.js";
-import {CRTDegaussConfig} from "../../my-nft-gen/src/effects/finalImageEffects/crtDegaussEvent/CRTDegaussConfig.js";
+import {CRTDegaussEffect} from "../../my-nft-gen/src/effects/keyFrameEffects/crtDegaussEvent/CRTDegaussEffect.js";
+import {CRTDegaussConfig} from "../../my-nft-gen/src/effects/keyFrameEffects/crtDegaussEvent/CRTDegaussConfig.js";
 import {FuzzyBandConfig} from "../../my-nft-gen/src/effects/primaryEffects/fuzzyBands/FuzzyBandConfig.js";
 import {FuzzyBandEffect} from "../../my-nft-gen/src/effects/primaryEffects/fuzzyBands/FuzzyBandEffect.js";
 import {Point2D} from "../../my-nft-gen/src/core/layer/configType/Point2D.js";
@@ -68,9 +68,9 @@ const createComposition = async (colorScheme) => {
                     effect: CRTDegaussEffect,
                     percentChance: 100,
                     currentEffectConfig: new CRTDegaussConfig({
-                        keyFrames: [1550],
-                        glitchFrameCount: [100, 120, 140],
-                        sectionHeight: [10, 15, 20, 60],
+                        keyFrames: [190],
+                        glitchFrameCount: [240],
+                        sectionHeight: [10, 15, 20, 40],
                         offset: {lower: 5, upper: 30},
                         direction: [-1, 1],
                         glitchTimes: {lower: 1, upper: 2},
@@ -86,13 +86,20 @@ const createComposition = async (colorScheme) => {
 
     const createRings = async () => {
 
+        const ringSpoke = 36;
+
         const outerRadius = 400;
         const secondRadiusReduction = 0.75;
         const secondRadius = outerRadius * secondRadiusReduction;
-        const ringSpoke = 36;
+
+        const thirdRadiusReduction = 0.5;
+        const thirdRadius = outerRadius * thirdRadiusReduction;
+
 
         const outerRingColor = '#00FF00';
         const innerRingColor = '#FF0000';
+        const thirdRingColor = '#FFFF00';
+
 
         for (let i = 0; i < 360; i = i + ringSpoke) {
             await myTestProject.addPrimaryEffect({
@@ -105,7 +112,7 @@ const createComposition = async (colorScheme) => {
                         underLayerOpacity: 0.5,
                         startAngle: {lower: 0, upper: 360},
                         numberOfRings: {lower: 8, upper: 8},
-                        stroke: 2,
+                        stroke: 1,
                         thickness: 0,
                         sparsityFactor: [60],
                         sequencePixelConstant: {
@@ -116,9 +123,42 @@ const createComposition = async (colorScheme) => {
                         minSequenceIndex: [10],
                         numberOfSequenceElements: [3],
                         speed: {lower: 2, upper: 2},
-                        accentRange: {bottom: {lower: 1, upper: 1}, top: {lower: 3, upper: 6}},
-                        blurRange: {bottom: {lower: 1, upper: 1}, top: {lower: 1, upper: 1}},
-                        featherTimes: {lower: 2, upper: 4},
+                        accentRange: {bottom: {lower:0, upper: 0}, top: {lower: 0, upper: 0}},
+                        blurRange: {bottom: {lower: 0, upper: 0}, top: {lower: 0, upper: 0}},
+                        featherTimes: {lower: 0, upper: 0},
+                        center: findPointByAngleAndCircle(new Point2D(1080 / 2, 1920 / 2), i, outerRadius),
+                        innerColor: new ColorPicker(ColorPicker.SelectionType.neutralBucket),
+                        outerColor: new ColorPicker(ColorPicker.SelectionType.color, outerRingColor),
+                    }),
+                }),
+            });
+        }
+
+        for (let i = 0; i < 360; i = i + ringSpoke) {
+            await myTestProject.addPrimaryEffect({
+                layerConfig: new LayerConfig({
+                    effect: EncircledSpiralEffect,
+                    percentChance: 100,
+                    currentEffectConfig: new EncircledSpiralConfig({
+                        invertLayers: true,
+                        layerOpacity: 0.55,
+                        underLayerOpacity: 0.5,
+                        startAngle: {lower: 0, upper: 360},
+                        numberOfRings: {lower: 8, upper: 8},
+                        stroke: 1,
+                        thickness:0,
+                        sparsityFactor: [60],
+                        sequencePixelConstant: {
+                            lower: (finalSize) => finalSize.shortestSide * 0.001,
+                            upper: (finalSize) => finalSize.shortestSide * 0.001,
+                        },
+                        sequence: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181],
+                        minSequenceIndex: [10],
+                        numberOfSequenceElements: [3],
+                        speed: {lower: 4, upper: 4},
+                        accentRange: {bottom: {lower:0, upper: 0}, top: {lower: 0, upper: 0}},
+                        blurRange: {bottom: {lower: 0, upper: 0}, top: {lower: 0, upper: 0}},
+                        featherTimes: {lower: 0, upper: 0},
                         center: findPointByAngleAndCircle(new Point2D(1080 / 2, 1920 / 2), i, secondRadius),
                         innerColor: new ColorPicker(ColorPicker.SelectionType.neutralBucket),
                         outerColor: new ColorPicker(ColorPicker.SelectionType.color, innerRingColor),
@@ -138,8 +178,8 @@ const createComposition = async (colorScheme) => {
                         underLayerOpacity: 0.5,
                         startAngle: {lower: 0, upper: 360},
                         numberOfRings: {lower: 8, upper: 8},
-                        stroke: 2,
-                        thickness: 0,
+                        stroke: 1,
+                        thickness:0,
                         sparsityFactor: [60],
                         sequencePixelConstant: {
                             lower: (finalSize) => finalSize.shortestSide * 0.001,
@@ -148,13 +188,13 @@ const createComposition = async (colorScheme) => {
                         sequence: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181],
                         minSequenceIndex: [10],
                         numberOfSequenceElements: [3],
-                        speed: {lower: 2, upper: 2},
-                        accentRange: {bottom: {lower: 1, upper: 1}, top: {lower: 3, upper: 6}},
-                        blurRange: {bottom: {lower: 1, upper: 1}, top: {lower: 1, upper: 1}},
-                        featherTimes: {lower: 2, upper: 4},
-                        center: findPointByAngleAndCircle(new Point2D(1080 / 2, 1920 / 2), i, outerRadius),
+                        speed: {lower: 6, upper: 6},
+                        accentRange: {bottom: {lower:0, upper: 0}, top: {lower: 0, upper: 0}},
+                        blurRange: {bottom: {lower: 0, upper: 0}, top: {lower: 0, upper: 0}},
+                        featherTimes: {lower: 0, upper: 0},
+                        center: findPointByAngleAndCircle(new Point2D(1080 / 2, 1920 / 2), i, thirdRadius),
                         innerColor: new ColorPicker(ColorPicker.SelectionType.neutralBucket),
-                        outerColor: new ColorPicker(ColorPicker.SelectionType.color, outerRingColor),
+                        outerColor: new ColorPicker(ColorPicker.SelectionType.color, thirdRingColor),
                     }),
                 }),
             });
@@ -169,14 +209,14 @@ const createComposition = async (colorScheme) => {
             percentChance: 100,
             currentEffectConfig: new MappedFramesConfig({
                 folderName: 'C:\\Users\\neomo\\WebstormProjects\\nft-scratch\\src\\assets\\mappedFrames\\all-seeing\\',
-                layerOpacity: [0.7],
+                layerOpacity: [0.95],
                 buffer: [200],
                 loopTimesMultiStep: [
                     new MultiStepDefinitionConfig({
                         minPercentage: 0,
                         maxPercentage: 40,
                         max: new Range(1, 1),
-                        times: new Range(3, 3),
+                        times: new Range(5, 5),
                     }),
                     new MultiStepDefinitionConfig({
                         minPercentage: 40,
@@ -188,7 +228,7 @@ const createComposition = async (colorScheme) => {
                         minPercentage: 60,
                         maxPercentage: 100,
                         max: new Range(1, 1),
-                        times: new Range(3, 3),
+                        times: new Range(5, 5),
                     })
                 ],
             }),
@@ -197,10 +237,10 @@ const createComposition = async (colorScheme) => {
                     effect: CRTDegaussEffect,
                     percentChance: 100,
                     currentEffectConfig: new CRTDegaussConfig({
-                        keyFrames: [100, 700, 1000, 1600],
-                        glitchFrameCount: [100, 120, 140],
-                        sectionHeight: [10, 15, 20, 60],
-                        offset: {lower: 5, upper: 30},
+                        keyFrames: [600],
+                        glitchFrameCount: [360],
+                        sectionHeight: [5, 15, 20],
+                        offset: {lower: 5, upper: 40},
                         direction: [-1, 1],
                         glitchTimes: {lower: 1, upper: 2},
                         backgroundRed: {lower: 0, upper: 0},
@@ -245,25 +285,14 @@ const createComposition = async (colorScheme) => {
                 shadowOpacityRange: {bottom: {lower: 0.7, upper: 0.7}, top: {lower: 0.9, upper: 0.9}},
                 linesOpacityRange: {bottom: {lower: 0.2, upper: 0.2}, top: {lower: 0.4, upper: 0.4}},
                 opacityTimes: {lower: 4, upper: 4},
-                lineRed: {lower: 128, upper: 128},
-                lineGreen: {lower: 128, upper: 128},
+                lineRed: {lower: 0, upper: 0},
+                lineGreen: {lower: 0, upper: 0},
                 lineBlue: {lower: 0, upper: 0},
-                lineHeight: {lower: 1, upper: 1},
+                lineHeight: {lower: 0.5, upper: 0.5},
                 edgePercentage: {lower: 0.20, upper: 0.20},
-                maxLineHeight: {lower: 3.5, upper: 3.5},
+                maxLineHeight: {lower: 3.75, upper: 3.75},
                 numberOfEdgeSections: {lower: 20, upper: 20},
-            }),
-            possibleSecondaryEffects: [
-                new LayerConfig({
-                    effect: GlowEffect,
-                    percentChance: 100,
-                    currentEffectConfig: new GlowConfig({
-                        lowerRange: {lower: 0, upper: 0},
-                        upperRange: {lower: 180, upper: 180},
-                        times: {lower: 1, upper: 1},
-                    }),
-                })
-            ]
+            })
         }),
     });
 
