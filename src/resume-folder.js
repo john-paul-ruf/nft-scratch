3b38ad2fc58c6383dc755d01587377ee494174ef
the.dude <john.paul.ruf@gmail.com>
2025-01-19 09:39:23 -0600

    making art

--- src/resume-folder.js
+++ src/resume-folder.js
@@ -1,8 +1,7 @@
 
 import {ResumeProject} from "../../my-nft-gen/src/app/ResumeProject.js";
 
-// src/key-frame/key-frame-dg8941p/key-frame-dg8941p-settings.json
-// src/key-frame/key-frame-km9gvvn/key-frame-km9gvvn-settings.json
+// src/key-frame/key-frame-wwosk2c/key-frame-wwosk2c-settings.json
 
 await ResumeProject(process.argv[2])
 
