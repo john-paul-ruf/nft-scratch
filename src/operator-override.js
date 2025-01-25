087e62dca51f796f17413132215df645eb681376
the.dude <john.paul.ruf@gmail.com>
2025-01-25 16:42:40 -0600

    making art

--- src/operator-override.js
+++ src/operator-override.js
@@ -23,6 +23,10 @@ import {
 import {ViewportEffect} from "../../my-nft-gen/src/effects/primaryEffects/viewport/ViewportEffect.js";
 import {ViewportConfig} from "../../my-nft-gen/src/effects/primaryEffects/viewport/ViewportConfig.js";
 import {ColorPicker} from "my-nft-gen/src/core/layer/configType/ColorPicker.js";
+import {CRTShadowEffect} from "my-nft-gen/src/effects/finalImageEffects/crtShadow/CRTShadowEffect.js";
+import {CRTShadowConfig} from "my-nft-gen/src/effects/finalImageEffects/crtShadow/CRTShadowConfig.js";
+import {CRTScanLinesEffect} from "my-nft-gen/src/effects/finalImageEffects/crtScanLines/CRTScanLinesEffect.js";
+import {CRTScanLinesConfig} from "my-nft-gen/src/effects/finalImageEffects/crtScanLines/CRTScanLinesConfig.js";
 
 
 const promiseArray = [];
@@ -33,7 +37,7 @@ const createComposition = async (colorScheme) => {
         projectName: 'operator-override',
         projectDirectory: 'src/key-frame',
         neutrals: ['#FFFFFF'],
-        backgrounds: ['#000000'],
+        backgrounds: ['#848484'],
         numberOfFrame: 1800,
         colorScheme: colorScheme,
         longestSideInPixels: 1920,
@@ -124,6 +128,50 @@ const createComposition = async (colorScheme) => {
                 ],
             }),
             possibleSecondaryEffects: [...createDegaussEffects({arraySize: 50})]
+        }),
+    });
+
+    await myTestProject.addFinalEffect({
+        layerConfig: new LayerConfig({
+            effect: CRTShadowEffect,
+            percentChance: 100,
+            currentEffectConfig: new CRTShadowConfig({
+                shadowOpacityRange: {bottom: {lower: 0.7, upper: 0.7}, top: {lower: 0.9, upper: 0.9}},
+                linesOpacityRange: {bottom: {lower: 0.6, upper: 0.6}, top: {lower: 0.9, upper: 0.9}},
+                opacityTimes: {lower: 2, upper: 2},
+                lineRed: {lower: 0, upper: 0},
+                lineGreen: {lower: 127, upper: 127},
+                lineBlue: {lower: 0, upper: 0},
+                lineHeight: {lower: 0.5, upper: 0.5},
+                edgePercentage: {lower: 0.15, upper: 0.15},
+                maxLineHeight: {lower: 4, upper: 4},
+                numberOfEdgeSections: {lower: 40, upper: 40},
+            })
+        }),
+    });
+
+    await myTestProject.addFinalEffect({
+        layerConfig: new LayerConfig({
+            effect: CRTScanLinesEffect,
+            percentChance: 100,
+            currentEffectConfig: new CRTScanLinesConfig({
+                lines: {lower: 100, upper: 100},
+                loopTimes: {lower: 1, upper: 3},
+                brightnessRange: {bottom: {lower: 10, upper: 15}, top: {lower: 20, upper: 25}},
+                brightnessTimes: {lower: 2, upper: 8},
+                thicknessRange: {bottom: {lower: 4, upper: 8}, top: {lower: 10, upper: 12}},
+                thicknessTimes: {lower: 2, upper: 8},
+                lineBlurRange: {bottom: {lower: 10, upper: 20}, top: {lower: 30, upper: 40}},
+                lineBlurTimes: {lower: 2, upper: 8},
+                colorTintRange: {
+                    redRange: {bottom: {lower: 0.5, upper: 0.6}, top: {lower: 0.7, upper: 0.8}},
+                    greenRange: {bottom: {lower: 1.5, upper: 1.6}, top: {lower: 1.7, upper: 1.8}},
+                    blueRange: {bottom: {lower: 0.5, upper: 0.6}, top: {lower: 0.7, upper: 0.8}},
+                },
+                colorTintTimes: {lower: 2, upper: 8},
+                opacityRange: {bottom: {lower: 0.3, upper: 0.4}, top: {lower: 0.5, upper: 0.6}},
+                opacityTimes: {lower: 2, upper: 8},
+            }),
         }),
     });
 
