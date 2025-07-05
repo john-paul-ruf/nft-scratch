c4a05145a0979f509dd6f90e06c26e2fac41d82e
the.phoenix <john.paul.ruf@gmail.com>
2025-07-05 11:11:07 -0500

    back online

--- src/resume-folder.js
+++ src/resume-folder.js
@@ -1,7 +1,7 @@
 
 import {ResumeProject} from "../../my-nft-gen/src/app/ResumeProject.js";
 
-// npm run resume-folder src/scratch/red-eye-reduction-bc4kq0z/red-eye-reduction-bc4kq0z-settings.json
+// npm run resume-folder src/scratch/red-eye-reduction-hvhmdql/red-eye-reduction-hvhmdql-settings.json
 
 await ResumeProject(process.argv[2])
 
