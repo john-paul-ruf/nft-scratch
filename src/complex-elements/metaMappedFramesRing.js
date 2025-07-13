38a03097a316a572e27a1b2299374c33bb2eb22c
the.phoenix <john.paul.ruf@gmail.com>
2025-07-13 12:15:30 -0500

    tweaks

--- src/complex-elements/metaMappedFramesRing.js
+++ src/complex-elements/metaMappedFramesRing.js
@@ -56,7 +56,7 @@ export const metaMappedFramesRing = async ({
 
     const multiStep = await generateCustomRandomMultistep({
         numberOfSegments: 15,
-        times: new Range(1, 5),
+        times: new Range(3, 8),
     });
 
     const ringMultiStep = await generateCustomRandomMultistep({
@@ -94,60 +94,34 @@ export const metaMappedFramesRing = async ({
                         {
                             arraySize: 75,
                             randomChance: {lower: 10, upper: 25},
-                            glitchFrameCount: {lower: 275, upper: 320},
-                            keyFrames: {lower: 0, upper: 1800 - 320},
+                            glitchFrameCount: {lower: 10, upper: 30},
+                            keyFrames: {lower: 0, upper: 1800 - 25},
                             sectionHeight: [1, 5, 10],
                             offset: {lower: 2, upper: 5},
                             direction: [-1, 1],
-                            glitchTimes: {lower: 3, upper: 8},
+                            glitchTimes: {lower: 1, upper: 1},
                         },
                         {
                             arraySize: 75,
                             randomChance: {lower: 10, upper: 25},
-                            glitchFrameCount: {lower: 25, upper: 75},
-                            keyFrames: {lower: 0, upper: 1800 - 75},
+                            glitchFrameCount: {lower: 5, upper: 10},
+                            keyFrames: {lower: 0, upper: 1800 - 25},
                             sectionHeight: [1, 2, 3],
                             offset: {lower: 5, upper: 10},
                             direction: [-1, 1],
-                            glitchTimes: {lower: 3, upper: 8},
+                            glitchTimes: {lower: 1, upper: 1},
                         },
                         {
                             arraySize: 75,
                             randomChance: {lower: 5, upper: 25},
-                            glitchFrameCount: {lower: 180, upper: 240},
-                            keyFrames: {lower: 0, upper: 1800 - 240},
+                            glitchFrameCount: {lower: 10, upper: 20},
+                            keyFrames: {lower: 0, upper: 1800 - 20},
                             sectionHeight: [1, 2, 5],
                             offset: {lower: 3, upper: 7},
                             direction: [-1, 1],
-                            glitchTimes: {lower: 3, upper: 8},
+                            glitchTimes: {lower: 1, upper: 1},
                         },
                     ]),
-                    ...createGlowEffects([
-                        {
-                            arraySize: 75,
-                            randomChance: {lower: 10, upper: 25},
-                            glitchFrameCount: {lower: 25, upper: 160},
-                            keyFrames: {lower: 0, upper: 1800 - 160},
-                            lowerRange: {lower: 4, upper: 8},
-                            times: {lower: 1, upper: 3},
-                        },
-                        {
-                            arraySize: 50,
-                            randomChance: {lower: 10, upper: 25},
-                            glitchFrameCount: {lower: 25, upper: 75},
-                            keyFrames: {lower: 0, upper: 1800 - 75},
-                            lowerRange: {lower: 2, upper: 6},
-                            times: {lower: 1, upper: 3},
-                        },
-                        {
-                            arraySize: 50,
-                            randomChance: {lower: 10, upper: 25},
-                            glitchFrameCount: {lower: 60, upper: 120},
-                            keyFrames: {lower: 0, upper: 1800 - 120},
-                            lowerRange: {lower: 1, upper: 4},
-                            times: {lower: 1, upper: 3},
-                        }
-                    ])
                 ],
             }),
         });
