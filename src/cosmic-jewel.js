50dba6274bd74a8705c62f8b68e43b16c833f597
the.phoenix <john.paul.ruf@gmail.com>
2025-09-06 16:40:20 -0500

    next comp

--- src/cosmic-jewel.js
+++ src/cosmic-jewel.js
@@ -22,7 +22,7 @@ import {FindValueAlgorithm} from "my-nft-gen/src/core/math/findValue.js";
 
 const promiseArray = [];
 const backgroundHex = '#080808'
-const scheme = neonCyberdream;
+const scheme = neonHarmony;
 
 const createComposition = async (colorScheme) => {
         const myTestProject = new Project({
